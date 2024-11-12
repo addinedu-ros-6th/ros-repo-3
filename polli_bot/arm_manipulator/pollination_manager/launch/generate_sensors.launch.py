04fa8c458b5843b0dd267a47f258d55461b44b29
smko0907 <0907smko@gmail.com>
2024-11-12 16:28:26 +0900

    modified cmd and cwd inside launch file of pollination_manager

--- polli_bot/arm_manipulator/pollination_manager/launch/generate_sensors.launch.py
+++ polli_bot/arm_manipulator/pollination_manager/launch/generate_sensors.launch.py
@@ -1,35 +1,36 @@
-# generate_sensors.launch.py
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
+import os
 
 def generate_launch_description():
+    workspace_dir = os.path.join(os.getenv('HOME'), 'ksm/src/pollination_manager')
+
     return LaunchDescription([
-        # Compile and run the C++ pointcloud generator executable
+        # Compile the C++ pointcloud generator executable
         ExecuteProcess(
-            cmd=['g++', 'src/pointcloud_generator.cpp', '-o', 'generator',
-                 '-lrealsense2', '-lboost_system', '`pkg-config --cflags --libs opencv4`'],
-                 #or just use 'bash', '-c', 'g++ src/pointcloud_generator.cpp -o generator -lrealsense2 -lboost_system `pkg-config --cflags --libs opencv4`'
+            cmd=['bash', '-c', 'g++ src/pointcloud_generator.cpp -o pointcloud_generator -lrealsense2 -lboost_system `pkg-config --cflags --libs opencv4`'],
+            cwd=workspace_dir,
             output='screen'
         ),
         
+        # Run the compiled C++ executable (assuming generator is in 'ksm' after compilation)
         ExecuteProcess(
-            cmd=['./generator'],
-            cwd='dev_ws',  # Specify the directory if needed
+            cmd=[os.path.join(workspace_dir, './pointcloud_generator')],
             output='screen'
         ),
         
-        # Run the Python script for sending RGB, depth frames via UDP
+        # Run the Python script for sending frames via UDP
         ExecuteProcess(
-            cmd=['python3', 'src/frame_generator.py'],
+            cmd=['python3', os.path.join(workspace_dir, 'src', 'frame_generator.py')],
             output='screen'
         ),
         
-        # ROS2 node for the publishing raw imu
+        # Launch the ROS2 node for publishing raw IMU data
         Node(
             package='pollination_manager',
-            executable='raw_ebimu_publisher',
-            name='raw_ebimu_pub',
+            executable='raw_ebimu_pub.py',
+            name='raw_ebimu_publisher',
             output='screen'
         )
     ])
